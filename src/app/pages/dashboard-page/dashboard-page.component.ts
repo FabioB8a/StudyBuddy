c95c77d9f654830140f937b05a1c047c1a7d697c
parraluisa <parran_ll@javeriana.edu.co>
2024-04-23 10:01:07 -0500

    Basic advance of dashboard component

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../../global/sidebar/sidebar.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
