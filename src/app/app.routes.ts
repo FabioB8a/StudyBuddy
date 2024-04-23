17fcd1b89a42c05e3951daef5baa5020fb5b0b57
parraluisa <parran_ll@javeriana.edu.co>
2024-04-23 09:59:53 -0500

    Basic route added for langing and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {DashboardPageComponent} from "./pages/dashboard-page/dashboard-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'dashboard', component: DashboardPageComponent},
+];
