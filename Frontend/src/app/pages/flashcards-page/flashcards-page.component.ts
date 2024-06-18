29312ab842c3a32aa851910c70a3462635396407
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-06-18 02:41:39 -0500

    NextCard

--- Frontend/src/app/pages/flashcards-page/flashcards-page.component.ts
+++ Frontend/src/app/pages/flashcards-page/flashcards-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {SidebarComponent} from "../../global/sidebar/sidebar.component";
 import {NgOptimizedImage} from "@angular/common";
 import { TopbarComponent } from '../../global/topbar/topbar.component';
@@ -16,6 +17,17 @@ import { TopbarComponent } from '../../global/topbar/topbar.component';
 })
 export class FlashcardsPageComponent {
 
+  // Datos de las flashcards
+
+  flashcards = [
+    { title: 'Flashcard 1', frontText: 'Texto de la cara delantera 1', backText: 'Texto de la cara trasera 1' },
+    { title: 'Flashcard 2', frontText: 'Texto de la cara delantera 2', backText: 'Texto de la cara trasera 2' },
+    { title: 'Flashcard 3', frontText: 'Texto de la cara delantera 3', backText: 'Texto de la cara trasera 3' },
+    { title: 'Flashcard 4', frontText: 'Texto de la cara delantera 4', backText: 'Texto de la cara trasera 4' },
+    { title: 'Flashcard 5', frontText: 'Texto de la cara delantera 5', backText: 'Texto de la cara trasera 5' },
+    { title: 'Flashcard 6', frontText: 'Texto de la cara delantera 6', backText: 'Texto de la cara trasera 6' }
+  ];
+
   // Flip de la flashcard
 
   flipped: boolean = false;
@@ -24,6 +36,12 @@ export class FlashcardsPageComponent {
     console.log("Card Flipped")
     this.flipped = !this.flipped;
   }
+
+  nextCard() {
+    // TO-DO
+    console.log("Next card")
+  }
+
 
 
   // Botones clickeados
